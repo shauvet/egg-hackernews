4e558fab48389e128ea410601135bc3865e7bee0
yangxiaowei <wangzhe23@gmail.com>
2018-06-19 22:30:12 +0800

    update news

--- config/config.default.js
+++ config/config.default.js
@@ -6,6 +6,17 @@ module.exports = appInfo => {
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1529244024422_8455';
 
+  config.view = {
+    defaultViewEngine: 'nunjucks',
+    defaultExtension: '.tpl',
+    mapping: {
+      '.tpl': 'nunjucks'
+      // '.xtpl': 'xtpl'
+    }
+  };
+
+  // config.xtpl = {};
+
   // add your config here
   config.middleware = [];
 
--- config/plugin.js
+++ config/plugin.js
@@ -2,3 +2,12 @@
 
 // had enabled by egg
 // exports.static = true;
+// exports.xtpl = {
+//   enabled: true,
+//   package: 'egg-view-xtpl'
+// };
+
+exports.nunjucks = {
+  enable: true,
+  package: 'egg-view-nunjucks'
+};
